e8646ef7ca186d17f754491eef4b0089d8143d8e
Greg <costagregorioalessio@gmail.com>
2018-12-01 20:14:54 +0000

    Refactor tests

--- src/shared/Components/Common/PageTitle/PageTitle.spec.jsx
+++ src/shared/Components/Common/PageTitle/PageTitle.spec.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 
 describe('PageTitle', () => {
   it('renders a h2 title', () => {
-    const component = shallow(<PageTitle  />)
+    const component = shallow(<PageTitle />)
+
     expect(component.find('h2').exists()).toBe(true)
   })
   it('renders title passed', () => {
     const mockTitle = 'mocktitle'
     const component = shallow(<PageTitle title={mockTitle} />)
+
     expect(component.find('h2').text()).toBe(mockTitle)
   })
 })
