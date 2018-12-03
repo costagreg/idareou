511663b7cc215acc7d9bfbc4c750006df82d5cef
Greg <costagregorioalessio@gmail.com>
2018-12-03 13:18:19 +0000

    Move form container to container fold

--- src/shared/AppRouter/AppRouter.js
+++ src/shared/AppRouter/AppRouter.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Button } from '../Components/Common/Button'
+import { TextArea } from '../Components/Common/TextArea'
+import { TextInput } from '../Components/Common/TextInput'
+import { FormContainer } from '../Containers/FormContainer'
+import { PageTitle } from '../Components/Common/PageTitle'
 import routes from './routes'
 
-if(process.browser) {
+if (process.browser) {
   require('../../styles/global.scss')
 }
 
@@ -11,17 +15,22 @@ const AppRouter = () => {
   return (
     <Fragment>
       <div className='container'>
-        <Button text={'test'} disabled={true} />
+        <FormContainer>
+          <PageTitle title='Create a bet' />
+          <TextInput placeholder='Title' />
+          <TextArea placeholder='Description' />
+          <Button text={'test'} disabled={true} />
+        </FormContainer>
         <Switch>
-            {
-              routes.map(({ Component, path }, index) =>
-                <Route
-                  key={index}
-                  exact
-                  path={path}
-                  component={Component}
-                />)
-            }
+          {
+            routes.map(({ Component, path }, index) =>
+              <Route
+                key={index}
+                exact
+                path={path}
+                component={Component}
+              />)
+          }
         </Switch>
       </div>
     </Fragment>
