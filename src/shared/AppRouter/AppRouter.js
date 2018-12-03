29734d19e066c0c2babebfe293a962819ff7c5a1
Greg <costagregorioalessio@gmail.com>
2018-12-03 13:18:19 +0000

    Move form container to container fold

--- src/shared/AppRouter/AppRouter.js
+++ src/shared/AppRouter/AppRouter.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Button } from '../Components/Common/Button'
+import { TextArea } from '../Components/Common/TextArea'
+import { TextInput } from '../Components/Common/TextInput'
+import { FormContainer } from '../Containers/FormContainer'
+import { PageTitle } from '../Components/Common/PageTitle'
 import routes from './routes'
 
 if (process.browser) {
@@ -11,7 +15,12 @@ const AppRouter = () => {
   return (
     <Fragment>
       <div className='container'>
-        <Button text={'test'} disabled={true} />
+        <FormContainer>
+          <PageTitle title='Create a bet' />
+          <TextInput placeholder='Title' />
+          <TextArea placeholder='Description' />
+          <Button text={'test'} disabled={true} />
+        </FormContainer>
         <Switch>
           {
             routes.map(({ Component, path }, index) =>
