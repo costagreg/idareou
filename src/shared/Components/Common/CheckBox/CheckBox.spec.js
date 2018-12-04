f71dd24fde2709f96537038186f835b699c728fb
Greg <costagregorioalessio@gmail.com>
2018-12-04 21:56:55 +0000

    Create checkbox component

---
+++ src/shared/Components/Common/CheckBox/CheckBox.spec.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import CheckBox from './CheckBox'
+import { shallow } from 'enzyme'
+
+describe('@CheckBox', () => {
+  it('renders a checkBox input as default', () => {
+    const component = shallow(<CheckBox />)
+
+    expect(component.find('input[type=\'checkbox\']').exists()).toBe(true)
+  })
+  describe('passing name as parameter', () => {
+    it('adds the name parameter to the element', () => {
+      const mockname = 'mockname'
+      const component = shallow(<CheckBox name={mockname} />)
+
+      expect(component.find('input[type=\'checkbox\']').prop('name')).toBe(mockname)
+    })
+  })
+  describe('passing text as parameter', () => {
+    it('adds a label to the checkbox', () => {
+      const mocktext = 'mocktext'
+      const component = shallow(<CheckBox text={mocktext} />)
+
+      expect(component.find('.CheckBox__Label').text()).toBe(mocktext)
+    })
+  })
+})
