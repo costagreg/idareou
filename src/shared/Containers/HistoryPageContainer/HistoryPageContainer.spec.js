e459e191cb1221db76332cb0a43c5bcd691874ea
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2019-01-23 21:57:14 +0000

    historyPage fetch on componentDidMount

--- src/shared/Containers/HistoryPageContainer/HistoryPageContainer.spec.js
+++ src/shared/Containers/HistoryPageContainer/HistoryPageContainer.spec.js
@@ -6,10 +6,21 @@ import { HistoryPageContainer } from './HistoryPageContainer'
 describe('given HistoryPageContainer component', () => {
   describe('when trying to render the HistoryPageContainer component', () => {
     it('should render the HistoryPageContainer component', () => {
+      const backUpDidMount = HistoryPageContainer.prototype.componentDidMount
+      HistoryPageContainer.prototype.componentDidMount = jest.fn()
       const component = shallow(<HistoryPageContainer />)
 
       expect(component.length).toBe(1)
       expect(component.find('HistoryPage').length).toBe(1)
+      HistoryPageContainer.prototype.componentDidMount = backUpDidMount
+    })
+    describe('when bets are empty', () => {
+      it('should try to fetch the bets', () => {
+        const fetchToStore = jest.fn()
+        shallow(<HistoryPageContainer fetchToStore={fetchToStore} />)
+
+        expect(fetchToStore).toHaveBeenCalled()
+      })
     })
   })
 })
