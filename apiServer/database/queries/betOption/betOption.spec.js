c4e9685c962e42ac72d84174e00f07066518a6c0
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2019-02-27 11:32:22 +0000

    betoption queries spec

--- apiServer/database/queries/betOption/betOption.spec.js
+++ apiServer/database/queries/betOption/betOption.spec.js
@@ -1,9 +1,40 @@
-// import { addBetOption } from './betOption'
+import { BetOption } from '../../models'
+
+import { addBetOption, findBetOption } from './betOption'
 
 describe('Betptions', () => {
-  describe('when calling addBetOptiopns', () => {
-    it('should add a new betoption into betoptions document', () => {
-      expect(0).toBe(0)
+  describe('when calling addBetOptiopns', async () => {
+    it('should add a new betoption into betoptions document when title is passed', async () => {
+      const newBet = { title: 'titleMock' }
+
+      const betCreated = await addBetOption(newBet)
+
+      const betSaved = await BetOption.findById(betCreated.id)
+
+      expect(betSaved.title).toBe(newBet.title)
+    })
+  })
+  describe('when trying to find bet', () => {
+    describe('when bet option id has been created already', () => {
+      it('should find the betoption', async () => {
+        const newBet = { title: 'titleMock' }
+
+        const betCreated = await BetOption.create(newBet)
+
+        const betFound = await findBetOption(betCreated.id)
+
+        expect(betFound._id).toEqual(betCreated._id)
+        expect(betFound.title).toEqual(betCreated.title)
+      })
+    })
+    describe('when bet option id has not been created already', () => {
+      it('should return null', async () => {
+        const newBet = { title: 'titleMock', _id: '847290dskdjs01' }
+
+        const betFound = await findBetOption(newBet.id)
+
+        expect(betFound).toBe(null)
+      })
     })
   })
 })
