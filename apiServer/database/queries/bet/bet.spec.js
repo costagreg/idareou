f3b6bf5e65dee6e18f80d29ca42517327c46efc4
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2019-02-27 15:19:35 +0000

    add test to bet mongoose queries

--- apiServer/database/queries/bet/bet.spec.js
+++ apiServer/database/queries/bet/bet.spec.js
@@ -1,35 +1,78 @@
-import { Bet, User } from '../../models'
-import { findBet } from './bet'
+import { Bet, BetOption } from '../../models'
+import { findBet, addBet, updateBetParticipant } from './bet'
 
-fdescribe('Bet queries', () => {
-  describe('addBet', async () => {
-  //   const bet = new Bet({
-  //     title: 'title',
-  //     description: 'description',
-  //     amount: 10,
-  //     currency: '£',
-  //     options: [],
-  //     participants: []
-  //   })
+describe('Bet queries', () => {
+  const bet = {
+    title: 'title',
+    description: 'description',
+    amount: 10.00,
+    currency: '$',
+    options: [],
+    participants: []
+  }
+
+  describe('addBet', () => {
+    describe('when currency is passed', () => {
+      it('should create a new bet with currency passed', async () => {
+        const betCreated = await addBet(bet)
+
+        const betFound = await Bet.findById(betCreated._id)
+
+        expect(betFound.title).toBe(bet.title)
+        expect(betFound.description).toBe(bet.description)
+        expect(betFound.amount).toBe(bet.amount)
+        expect(betFound.currency).toBe(bet.currency)
+        expect(betFound.options.length).toEqual(bet.options.length)
+        expect(betFound.participants.length).toEqual(bet.participants.length)
+      })
+    })
+    describe('when currency is not passed', () => {
+      it('should create a new bet with default currency £', async () => {
+        const betCreated = await addBet({ ...bet, currency: undefined })
+
+        const betFound = await Bet.findById(betCreated._id)
+
+        expect(betFound.title).toBe(bet.title)
+        expect(betFound.description).toBe(bet.description)
+        expect(betFound.amount).toBe(bet.amount)
+        expect(betFound.currency).toBe('£')
+        expect(betFound.options.length).toEqual(bet.options.length)
+        expect(betFound.participants.length).toEqual(bet.participants.length)
+      })
+    })
   })
   describe('findBet', () => {
-    it('finds bet by id', async () => {
-      const user = new User({
-        username: 'mockUsername'
-      })
+    it('finds bet by id if exist', async () => {
+      const betCreated = await addBet(bet)
 
-      await user.save()
+      const betFound = await findBet(betCreated._id)
 
-      const bet = new Bet({
-        title: 'mockTitle',
-        users: [user.id]
-      })
-      await bet.save()
+      expect(betFound._id).toEqual(betCreated._id)
+      expect(betFound.title).toBe(betCreated.title)
+      expect(betFound.amount).toBe(betCreated.amount)
+      expect(betFound.currency).toBe(bet.currency)
+      expect(betFound.options.length).toEqual(bet.options.length)
+      expect(betFound.participants.length).toEqual(bet.participants.length)
+    })
+  })
+  describe('updateBetParticipant', () => {
+    it('should update a bet created with userId and optionId as a participan', async () => {
+      const betCreated = await Bet.create(bet)
 
-      const betFound = await findBet(bet.id)
-      expect(betFound.id).toEqual(bet.id)
-      expect(betFound.title).toEqual('mockTitle')
-      expect(betFound.participate).not.toBe(null)
+      const newBetOption = await BetOption.create({ title: 'newTitle' })
+
+      const newUser = await BetOption.create({ username: 'newUser' })
+
+      const beforeBeforeUpdate = await Bet.findById(betCreated._id)
+
+      expect(beforeBeforeUpdate.participants.length).toBe(bet.participants.length)
+
+      await updateBetParticipant(betCreated._id, newUser._id, newBetOption._id)
+
+      const betAfterUpdate = await Bet.findById(betCreated._id)
+
+      expect(betAfterUpdate.participants[0].optionId).toEqual(newBetOption._id)
+      expect(betAfterUpdate.participants[0].userId).toEqual(newUser._id)
     })
   })
 })
