097816f1ea2c756561799e4d7a0d24fb74b264fb
Greg <costagregorioalessio@gmail.com>
2019-04-15 11:23:09 +0100

    Update tests

--- src/shared/Containers/HistoryPageContainer/HistoryPageContainer.spec.js
+++ src/shared/Containers/HistoryPageContainer/HistoryPageContainer.spec.js
@@ -3,14 +3,26 @@ import { shallow } from 'enzyme'
 
 import { HistoryPageContainer } from './HistoryPageContainer'
 
+const graphQlData = {
+  currentBets: [{
+    id: 'firstbet',
+    title: 'mybet'
+  }]
+}
+
 describe('given HistoryPageContainer component', () => {
   describe('when trying to render the HistoryPageContainer component', () => {
     it('should render the HistoryPageContainer component', () => {
-      const graphQlData = [{ currentBets: [] }]
       const component = shallow(<HistoryPageContainer data={graphQlData} />)
 
       expect(component.length).toBe(1)
-      expect(component.find('HistoryPage').length).toBe(1)
+    })
+    it('should render accordions with HistoryBetCard', () => {
+      const graphQlData = [{ currentBets: [] }]
+      const component = shallow(<HistoryPageContainer data={graphQlData} />)
+
+      expect(component.find('Accordion').exists()).toBe(true)
+      expect(component.find('Accordion HistoryBetCard').exists()).toBe(true)
     })
   })
 })
--- src/shared/Pages/BetPage/BetPage.spec.js
+++ src/shared/Pages/BetPage/BetPage.spec.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-
 import BetPage from './BetPage'
+
+jest.mock('../../Containers/BetPageContainer', () => function BetPageContainer() {})
 
 describe('BetPage', () => {
   describe('when trying to render the component', () => {
@@ -9,6 +10,16 @@ describe('BetPage', () => {
       const component = shallow(<BetPage />)
 
       expect(component.length).toBe(1)
+    })
+    it('should render a pagetitle', () => {
+      const component = shallow(<BetPage />)
+
+      expect(component.find('PageTitle').length).toBe(1)
+    })
+    it('should render a pagetitle', () => {
+      const component = shallow(<BetPage />)
+
+      expect(component.find('BetPageContainer').length).toBe(1)
     })
   })
 })
