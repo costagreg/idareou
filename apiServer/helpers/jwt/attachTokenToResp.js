3df5288a390d162267aead7562fd6a4666435eb7
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2019-04-23 10:06:32 +0100

    cookie expires same date as token

--- apiServer/helpers/jwt/attachTokenToResp.js
+++ apiServer/helpers/jwt/attachTokenToResp.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 export default (data, context) => {
   const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '30d' })
   context.res.cookie('token', token, {
-    maxAge: 1000 * 60 * 60 * 26,
+    maxAge: 30 * 60 * 60 * 24,
     domain: process.env.COOKIE_DOMAIN,
     httpOnly: false,
     secure: false
